dacbfe22d55a4311679750f837939275ed96f6d9
ramya <soumya.tipp@gmail.com>
2020-09-09 10:40:32 +0530

    commited BDD framework code first time from eclipse

--- ramya_project1/src/examples/Dropdown.java
+++ ramya_project1/src/examples/Dropdown.java
@@ -1,23 +1,17 @@
 package examples;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import ram.pack1.UtilityMethod;
@@ -29,7 +23,9 @@ public class Dropdown
 	public void setup()
 	{
 		System.setProperty("webdriver.chrome.driver","E:\\Selenium\\chromedriver.exe");
-		driver=new ChromeDriver();
+		ChromeOptions op=new ChromeOptions();
+		op.addArguments("--disable-inforbars");
+		driver=new ChromeDriver(op);
         driver.get("https://www.facebook.com");
         driver.manage().timeouts().pageLoadTimeout(10,TimeUnit.SECONDS);
         driver.manage().window().maximize();
